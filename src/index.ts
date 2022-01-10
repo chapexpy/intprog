f843b6ccd35ff0e4cddf2fe9cfe2a7062a3aed9d
chapexpy <enes@rubiklabs.com>
2022-01-10 23:12:26 +0300

    added chart update

--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,10 @@ import * as axios from 'axios'
 const method: axios.Method = 'GET'
 const options = {
   method,
-  url: 'https://fixer-fixer-currency-v1.p.rapidapi.com/latest',
-  params: { base: 'USD', symbols: 'GBP,JPY,EUR' },
+  url: 'https://covid-19-data.p.rapidapi.com/country/code',
+  params: { code: 'it' },
   headers: {
-    'x-rapidapi-host': 'fixer-fixer-currency-v1.p.rapidapi.com',
+    'x-rapidapi-host': 'covid-19-data.p.rapidapi.com',
     'x-rapidapi-key': '673d338e87msh89fc3788b48c076p12ec3djsnbac8f4d30272'
   }
 };
@@ -20,21 +20,40 @@ const options = {
   const app = express()
   const httpServer = http.createServer(app)
   const io = new Server(httpServer)
-
+  app.use(express.static(path.join(__dirname, 'public')))
   app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'))
   })
 
   io.on('connection', (socket) => {
     console.log('A user connected')
-    socket.on('clicked', () => {
-      axios.default.request(options).then((response) => {
-        socket.emit('update', (response.data))
-        console.log(response.data)
-      }).catch((error) => {
-        console.error(error)
+    socket.on('getChartData', (values) => {
+      console.log(values)
+      const data = []
+
+      values.forEach((element) => {
+        axios.default.request({
+          method,
+          url: 'https://covid-19-data.p.rapidapi.com/country/code',
+          params: { code: element },
+          headers: {
+            'x-rapidapi-host': 'covid-19-data.p.rapidapi.com',
+            'x-rapidapi-key': '673d338e87msh89fc3788b48c076p12ec3djsnbac8f4d30272'
+          }
+        }).then((response) => {
+          data.push(response.data)
+        }).catch((error) => {
+          console.error(error)
+        })
+        async function sleep () {
+          await new Promise((r) => setTimeout(r, 5000))
+        }
+        sleep()
       })
+      socket.emit('updateChartData', (data))
+      console.log(data)
     })
+
     // Whenever someone disconnects this piece of code executed
     socket.on('disconnect', () => {
       console.log('A user disconnected')
